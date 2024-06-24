1bc4f9f78930df6bc089a7afbc2eae8bb1f56a82
amine abaidi <amineabaidi546@gmail.com>
2024-06-24 14:15:13 +0100

    update liecnse

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ const App = () => {
         <About />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
